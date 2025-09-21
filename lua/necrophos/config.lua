d146c65ae4da56cb526d1a746abed0a181de91be
electromidz <electromidz@gmail.com>
2025-09-21 22:50:10 +0330

    work

--- lua/necrophos/config.lua
+++ lua/necrophos/config.lua
@@ -1,15 +1,21 @@
 local M = {}
 
-M.default_theme = "necrophos"
-M.transparent_background = false
-M.italic_comments = true
+M.config = {
+	theme = "necrophos", -- default theme
+	transparent = false,
+	italic_comments = true,
+}
 
--- Setup function for users to call
-function M.setup(opts)
-	opts = opts or {}
-	M.default_theme = opts.default_theme or M.default_theme
-	M.transparent_background = opts.transparent_background or M.transparent_background
-	M.italic_comments = opts.italic_comments or M.italic_comments
+-- -- Setup function for users to call
+-- function M.setup(opts)
+-- 	opts = opts or {}
+-- 	M.default_theme = opts.default_theme or M.default_theme
+-- 	M.transparent_background = opts.transparent_background or M.transparent_background
+-- 	M.italic_comments = opts.italic_comments or M.italic_comments
+-- end
+
+M.setup = function(opts)
+	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 end
 
 return M
